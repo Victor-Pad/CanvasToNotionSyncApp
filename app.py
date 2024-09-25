944b543dbca41bbee84a5f0265f3f1243b1ddd13
Kirash39 <113220462+Kirash39@users.noreply.github.com>
2024-09-25 03:16:59 -0400

    Add methods to flask and add method to get secret key

--- app.py
+++ app.py
@@ -1,8 +1,21 @@
-from flask import Flask
+from flask import Flask, render_template, redirect, url_for
+import os
+
+
+
+def load_env_file():
+    if os.path.exists('.env'):
+        with open('.env') as f:
+            for line in f:
+                if line.strip():
+                    key, value = line.strip().split('=', 1)
+                    os.environ[key] = value
+
+# Load the .env file
+load_env_file()
 
 app = Flask(__name__)
-
-
+app.config["SECRET_KEY"] = os.getenv('SECRET_KEY')
 @app.route('/')
 def hello_world():  # put application's code here
     return 'Hello World!'
