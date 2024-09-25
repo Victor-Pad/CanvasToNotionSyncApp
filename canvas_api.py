d1f336f90b10197eee83091e715f0ec600c8d4de
Kirash39 <113220462+Kirash39@users.noreply.github.com>
2024-09-25 03:29:44 -0400

    Add class to detect errors in the API call

--- canvas_api.py
+++ canvas_api.py
@@ -1,3 +1,8 @@
 import requests
-from config import CANVAS_API_URL,CANVAS_API_TOKEN
+from config import CANVAS_API_URL, CANVAS_API_TOKEN
 from datetime import datetime
+
+class CanvasAPIError(Exception):
+    """Exception raised for errors in the Canvas' API call"""
+    pass
+
